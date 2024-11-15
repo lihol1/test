b1ffc0dd7bb74f10738b4cd4cc1ee2388488fca6
lihol1 <lihol1@mail.ru>
2024-11-15 07:46:03 +0300

    bugfix: remove onDelete

--- src/components/MaterialUI.jsx
+++ src/components/MaterialUI.jsx
@@ -497,7 +497,7 @@ export default function MaterialUI () {
                 renderValue={(selected) => (
                     <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
                     {selected.map((value) => (
-                        <Chip key={value} label={value}  variant="filled" onClick={handleClick} onDelete={()=>handleDelete}/>
+                        <Chip key={value} label={value}  variant="filled" onClick={handleClick} />
                     ))}
                     </Box>
                 )}
